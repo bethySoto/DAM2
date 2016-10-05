934bce9e40ef4bfa542d30be4a5a28180010f90a
bethy <bethy@192.168.1.79>
2016-10-05 14:20:01 +0200

    TY3

---
+++ primos/src/primos/Primos.java
@@ -0,0 +1,24 @@
+package primos;
+
+import java.util.Scanner;
+
+public class Primos {
+	public static Scanner numero=new Scanner (System.in);
+	public static int numeroIntro;
+	public static void main(String[] args) {
+		System.out.println("Introduce un numero");
+		numeroIntro=numero.nextInt();
+		for(int f=1;f<numeroIntro;f++){
+			boolean primo=true;
+			for(int c=2;c<f;c++){
+				if(f%c==0){
+					primo=false;
+				}
+			}
+			if(primo){
+				System.out.println("Primo"+f);
+			}
+		}
+	}
+
+}
