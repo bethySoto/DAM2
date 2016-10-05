fc71cb79abb5d4ccadd7fed283473de81e579a31
bethySoto <raquel-87_@hotmail.com>
2016-10-05 14:21:29 +0200

    Update Primos.java

--- primos/src/primos/Primos.java
+++ primos/src/primos/Primos.java
@@ -6,6 +6,8 @@ public class Primos {
 	public static Scanner numero=new Scanner (System.in);
 	public static int numeroIntro;
 	public static void main(String[] args) {
+		
+		asaisoñfhaioshfoahisdoñasfd
 		System.out.println("Introduce un numero");
 		numeroIntro=numero.nextInt();
 		for(int f=1;f<numeroIntro;f++){
